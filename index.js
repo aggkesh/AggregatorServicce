705430cc5402b940676f9a909b0804c2eaf9c92f
keshav aggarwal <aggarwalkeshav38@gmail.com>
2020-03-29 17:17:12 +0530

    added code and coode clean up

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const port = 8082
+const request = require('request');
+const usersource = process.env.QUOTES_URL||"http://localhost:8080";
+const ordersource = process.env.QUOTES_URL||"http://localhost:8081";
+
+app.get('/orderdetails/:id', (req, res) => {
+	request(usersource+'/user/'+req.params.id, { json: true }, (usererr, userresp, userbody) => {
+	  if (usererr || userresp.statusCode != 200) {
+	  	 	res.status(500)
+	  	  	res.send("Error while getting users from "+usersource) 
+	  } else{
+	  	request(ordersource+'/order/'+req.params.id, { json: true }, (ordererr, orderresp, orderbody) => {
+	  		if (ordererr || orderresp.statusCode != 200) {
+	  			res.send(500)
+	  	  		res.send("Error while getting orders from "+ordersource) 
+	  		} else{
+	  			res.status(200)
+	  		  	res.send({
+		  			"userDetails":userbody,
+		  			"orderDetails":orderbody
+		  		});
+	  		}
+	  	});
+	  }
+	});
+})
+
+
+app.use(express.static('public'))
+
+app.listen(port, () => console.log(`Aggregator-Service app listening on port ${port}!`))
