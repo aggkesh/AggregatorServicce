ca94912f91627e59cb24f82aa8552e09485662fb
keshav aggarwal <aggarwalkeshav38@gmail.com>
2020-03-30 10:41:30 +0530

    added docker ignore & Dockerfile and code clean up

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express')
 const app = express()
 const port = 8082
 const request = require('request');
-const usersource = process.env.QUOTES_URL||"http://localhost:8080";
-const ordersource = process.env.QUOTES_URL||"http://localhost:8081";
+	const usersource = process.env.QUOTES_USER_URL || "http://userservice:8080";
+	const ordersource = process.env.QUOTES_ORDER_URL || "http://orderservice:8081";
 
 app.get('/orderdetails/:id', (req, res) => {
+	
 	request(usersource+'/user/'+req.params.id, { json: true }, (usererr, userresp, userbody) => {
 	  if (usererr || userresp.statusCode != 200) {
 	  	 	res.status(500)
@@ -13,7 +14,7 @@ app.get('/orderdetails/:id', (req, res) => {
 	  } else{
 	  	request(ordersource+'/order/'+req.params.id, { json: true }, (ordererr, orderresp, orderbody) => {
 	  		if (ordererr || orderresp.statusCode != 200) {
-	  			res.send(500)
+	  			res.status(500)
 	  	  		res.send("Error while getting orders from "+ordersource) 
 	  		} else{
 	  			res.status(200)
